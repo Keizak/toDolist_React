2604f094d76690ebf41bd94c8f5b6b00c8cc6bfe
Keizak <keizak@yandex.ru>
2021-01-11 11:45:07 +0300

    Updates

---
+++ precache-manifest.7c44491d3d44732db8a85257cc6495e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6515b69525cff198919ee4b8afda6ee",
+    "url": "/toDolist_React/index.html"
+  },
+  {
+    "revision": "3e7b05b30d9888f4e865",
+    "url": "/toDolist_React/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "c7034c2f194c33848919",
+    "url": "/toDolist_React/static/js/2.47e8bad0.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/toDolist_React/static/js/2.47e8bad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e7b05b30d9888f4e865",
+    "url": "/toDolist_React/static/js/main.8d69965d.chunk.js"
+  },
+  {
+    "revision": "72ea66a01e91871f6ac6",
+    "url": "/toDolist_React/static/js/runtime-main.ff04dd55.js"
+  }
+]);
